96f26f739ff475795af9e020b4c5e98a283543a8
Emre86 <emre.gun86@gmail.com>
2021-10-24 18:40:12 +0200

    Refactor du code avec l'ajout du service converter dans les composants

--- src/app/arabic-number/arabic-number.component.ts
+++ src/app/arabic-number/arabic-number.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ConverterService } from '../converter/converter.service';
 
 @Component({
   selector: 'app-arabic-number',
@@ -8,61 +10,20 @@ import { FormControl } from '@angular/forms';
 })
 export class ArabicNumberComponent implements OnInit {
 
-  @Input() romanNumbers = [""];
+  arabicDigit = 0;
 
-  arabicNumber = -9;
-  private romanMap: Map<string, number>;
-
-  constructor() {
-    this.romanMap = new Map();
-    this.romanMap.set("", 0)
-    this.romanMap.set("I", 1);
-    this.romanMap.set("II", 2);
-    this.romanMap.set("III", 3);
-    this.romanMap.set("IV", 4);
-    this.romanMap.set("V", 5);
-    this.romanMap.set("VI", 6);
-    this.romanMap.set("VII", 7);
-    this.romanMap.set("VIII", 8);
-    this.romanMap.set("IX", 9);
-    this.romanMap.set("X", 10);
-    this.romanMap.set("XX", 20);
-    this.romanMap.set("XXX", 30);
-    this.romanMap.set("XL", 40);
-    this.romanMap.set("L", 50);
-    this.romanMap.set("LX", 60);
-    this.romanMap.set("LXX", 70);
-    this.romanMap.set("LXXX", 80);
-    this.romanMap.set("XC", 90);
-    this.romanMap.set("C", 100);
-    this.romanMap.set("CC", 200);
-    this.romanMap.set("CCC", 300);
-    this.romanMap.set("CD", 400);
-    this.romanMap.set("D", 500);
-    this.romanMap.set("DC", 600);
-    this.romanMap.set("DCC", 700);
-    this.romanMap.set("DCCC", 800);
-    this.romanMap.set("CM", 900);
-    this.romanMap.set("M", 1000);
-    this.romanMap.set("MM", 2000);
-    this.romanMap.set("MMM", 3000);
-    this.romanMap.set("MMMM", 4000);
-  }
-
-  convertToArabic(romanNumbers: Array<string>): number {
-    let result = 0;
-
-    for (let entry of romanNumbers) {
-      result += this.romanMap.get(entry)!;
-    }
-
-    return result;
+  subscription: Subscription;
+  constructor(private converter: ConverterService) {
+    this.subscription = Subscription.EMPTY;
   }
 
   ngOnInit(): void {
+    this.subscription = this.converter.receiveNumber().subscribe((digit) => {
+      this.arabicDigit = digit
+    });
   }
 
-  ngOnChanges() {
-    this.arabicNumber = this.convertToArabic(this.romanNumbers)
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/roman-number/roman-number.component.ts
+++ src/app/roman-number/roman-number.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ConverterService } from '../converter/converter.service';
 
 
 @Component({
@@ -9,11 +10,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class RomanNumberComponent implements OnInit {
 
-  @Output() eventRoman = new EventEmitter<Array<string>>()
-
   public formGroupRoman: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private converter: ConverterService) {
     this.formGroupRoman = this.formBuilder.group({
       roman: []
     });
@@ -24,13 +23,8 @@ export class RomanNumberComponent implements OnInit {
 
   sendRomanNumber() {
     if (this.formGroupRoman.get("roman")!.value) {
-      let input = this.formGroupRoman.get('roman')?.value as String
-      const regex = new RegExp('^(M{0,4})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$');
-      let romanNumbers = input.match(regex);
-      romanNumbers?.shift()
-      if (romanNumbers) {
-        this.eventRoman.emit(romanNumbers);
-      }
+      let digits = this.formGroupRoman.get('roman')?.value as string
+      this.converter.sendNumber(digits)
     }
   }
 
